3353267c1599d33470048a402234d04552055372
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2021-09-05 22:16:37 +0200

    Seperate rules.mk and add chording for symbols

--- ergomin/qmk/config.h
+++ ergomin/qmk/config.h
@@ -13,6 +13,14 @@
 #define SECONDARY_ROW_PINS_ARRAY { (1<<7), (1<<4), (1<<3), (1<<2) }
 #define DIODE_DIRECTION COL2ROW
 
+// usb settings
+#define USB_POLLING_INTERVAL_MS 1
+#define FORCE_NKRO
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0xC6AF
+#define DEVICE_VER 0x0000
+#define PRODUCT Ergomin
+
 
 // timing settings
 #define DEBOUNCE 10
@@ -23,16 +31,12 @@
 #define ONESHOT_TAP_TOGGLE 2
 #define ONESHOT_TIMEOUT 200
 
-// usb settings
-#define USB_POLLING_INTERVAL_MS 1
-#define FORCE_NKRO
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0xC6AF
-#define DEVICE_VER 0x0000
-#define PRODUCT Ergomin
-
 // unicode input mode
 #define UNICODE_SELECTED_MODES UC_LNX
+
+// combo settings
+#define COMBO_VARIABLE_LEN
+#define COMBO_TERM 30
 
 // disable unused modules
 #define NO_ACTION_MACRO
---
+++ ergomin/qmk/keymaps/julianschuler/rules.mk
@@ -0,0 +1,22 @@
+# debounce options
+DEBOUNCE_TYPE = sym_eager_pk
+
+# build options, chango to "no" to disable
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+MIDI_ENABLE = no            # MIDI support
+UNICODE_ENABLE = yes        # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+ENCODER_ENABLE = no         # Enable the encoder
+COMBO_ENABLE = yes			# Enable key combos
+
+VPATH += keyboards/gboards
