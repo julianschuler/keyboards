2189b3e47237a605fdedeeed409ed9430562dcb9
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2021-12-25 01:30:07 +0100

    Improve plugin code structure and documentation

--- concavum/pcb/generate_pcb.py
+++ concavum/pcb/generate_pcb.py
@@ -7,6 +7,7 @@ from ast import literal_eval
 
 class GeneratePcb(pcbnew.ActionPlugin):
     def defaults(self):
+        """Set name and properties of the Plugin"""
         self.name = "Generate key matrix pcb"
         self.category = "Utils"
         self.description = "Generate the key matrix pcb from the openscad script"
@@ -16,11 +17,11 @@ class GeneratePcb(pcbnew.ActionPlugin):
         self.scad_file = os.path.join(os.path.dirname(__file__), "concavum-case.scad")
         self.dxf_file = os.path.join(os.path.dirname(__file__), "pcb_outline.dxf")
         self.origin_offset = [200, 100]
-        self.key_offset = [2.54, -5.08]
 
     def Run(self):
+        """Method to be called when the plugin is executed"""
         self.board = pcbnew.GetBoard()
-
+        # use openscad to generate the pcb outline as dxf
         cmd = [
             "openscad",
             "-D",
@@ -30,39 +31,43 @@ class GeneratePcb(pcbnew.ActionPlugin):
             self.scad_file,
         ]
         scad_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-        pos_list = literal_eval(scad_output.splitlines()[0][6:].decode())
-
-        self.draw_scad_dxf(self.dxf_file, pcbnew.Edge_Cuts)
-
-        # automatic delete causes a segfault, everything has to be deleted manually
-        # board.DeleteAllFootprints()
-
-        off = list(map(sum, zip(self.origin_offset, self.key_offset)))
-        for pos in [p for pos in pos_list for p in pos]:
-            key = pcbnew.FootprintLoad(
-                self.footprint_path + self.footprint_lib, self.footprint_name
-            )
-            p = pcbnew.wxPoint(
-                pcbnew.FromMM(off[0] - pos[0]), pcbnew.FromMM(off[1] - pos[1])
-            )
-            key.SetPosition(p)
-            self.board.Add(key)
-
-    def draw_scad_dxf(self, dxf_file, layer):
-        dxf = dxfgrabber.readfile(dxf_file)
+        # get key positions from openscad output
+        scad_values = literal_eval(scad_output.splitlines()[0][6:].decode())
+        # import the previously generated dxf as the pcb outline
         off = self.origin_offset
+        self.draw_dxf_lines(self.dxf_file, pcbnew.Edge_Cuts, off)
+        # add all keys in the finger cluster
+        for pos in [p for pos in scad_values[0] for p in pos]:
+            pcbnew.FromMM(off[0] - pos[0]), pcbnew.FromMM(off[1] - pos[1])
+            self.add_key(pos[0] + off[0], pos[1] + off[1])
+
+    def draw_dxf_lines(self, dxf_file, layer, off=[0, 0]):
+        """Draw lines from a given dxf file to a specific layer"""
+        dxf = dxfgrabber.readfile(dxf_file)
         for e in [e for e in dxf.entities if e.dxftype == "LINE"]:
             start = (off[0] + e.start[0], off[1] - e.start[1])
             end = (off[0] + e.end[0], off[1] - e.end[1])
             self.draw_line(start, end, layer)
 
     def draw_line(self, start, end, layer):
+        """Draw a line to the given layer specified by its start and end point"""
         seg = pcbnew.PCB_SHAPE(self.board)
         seg.SetLayer(layer)
         seg.SetShape(pcbnew.S_SEGMENT)
         seg.SetStart(pcbnew.wxPoint(*map(pcbnew.FromMM, start)))
         seg.SetEnd(pcbnew.wxPoint(*map(pcbnew.FromMM, end)))
         self.board.Add(seg)
+
+    def add_key(self, x, y, rotation=0):
+        """Add a single key to a given (x, y) position"""
+        off = [2.54, -5.08]
+        key = pcbnew.FootprintLoad(
+            self.footprint_path + self.footprint_lib, self.footprint_name
+        )
+        pos = pcbnew.wxPoint(pcbnew.FromMM(x + off[0]), pcbnew.FromMM(y + off[1]))
+        key.Rotate(pcbnew.wxPoint(0, 0), rotation * 10)
+        key.SetPosition(pos)
+        self.board.Add(key)
 
 
 GeneratePcb().register()
