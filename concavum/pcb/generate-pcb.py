d31ece57e9b63c954fc9c278a97a947566644f9a
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-01-15 00:08:11 +0100

    Add column connector tracks

--- concavum/pcb/generate-pcb.py
+++ concavum/pcb/generate-pcb.py
@@ -20,10 +20,11 @@ class GeneratePcb(pcbnew.ActionPlugin):
         self.footprint_name = "key-switch"
         self.scad_file = os.path.join(f_dir, "../case/concavum-case.scad")
         self.dxf_file = os.path.join(f_dir, "outline-key-matrix.dxf")
-        self.origin_offset = [195, 90]
+        self.origin_offset = np.array([195, 90])
         self.max_rows = 6
         self.max_cols = 8
         self.track_width = pcbnew.FromMM(0.3)
+        self.arc_segments = 120
 
     def Run(self, board_file=None):
         """Method to be called when the plugin is executed"""
@@ -58,10 +59,12 @@ class GeneratePcb(pcbnew.ActionPlugin):
         self.add_thumb_cluster(
             scad_vals[1], scad_vals[2], scad_vals[3], row_nets[-1], col_nets
         )
+        # add tracks going through the column connectors
+        self.add_col_connector_tracks(scad_vals[4], len(scad_vals[0][0]))
 
     def add_finger_cluster(self, finger_vals, row_nets, col_nets):
         """Add keys to the finger cluster"""
-        off = np.array(self.origin_offset)
+        off = self.origin_offset
         for i, col in enumerate(finger_vals):
             for j, pos in enumerate(col):
                 ref = f"SW{i+1}{j+1}"
@@ -71,14 +74,13 @@ class GeneratePcb(pcbnew.ActionPlugin):
 
     def add_thumb_cluster(self, t_rot, t_off, thumb_vals, row_net, col_nets):
         """Add keys to the thumb cluster"""
-        off = self.origin_offset
         t_rot_rad = np.radians(t_rot)
         t_sin, t_cos = np.sin(t_rot_rad), np.cos(t_rot_rad)
         for i, t_pos in enumerate(thumb_vals):
             pos = np.array(
                 (
-                    t_sin * t_pos[1] + t_off[0] + off[0],
-                    t_cos * t_pos[1] - t_off[1] + off[1],
+                    t_sin * t_pos[1] + t_off[0] + self.origin_offset[0],
+                    t_cos * t_pos[1] - t_off[1] + self.origin_offset[1],
                 )
             )
             col = len(thumb_vals) - 1 - i
@@ -92,12 +94,12 @@ class GeneratePcb(pcbnew.ActionPlugin):
                     t_rot + 180,
                 )
 
-    def draw_dxf_lines(self, dxf_file, layer, off=[0, 0]):
+    def draw_dxf_lines(self, dxf_file, layer, off=np.zeros(2)):
         """Draw lines from a given dxf file to a specific layer"""
         dxf = dxfgrabber.readfile(dxf_file)
         for e in [e for e in dxf.entities if e.dxftype == "LINE"]:
-            start = (off[0] + e.start[0], off[1] - e.start[1])
-            end = (off[0] + e.end[0], off[1] - e.end[1])
+            start = np.array((off[0] + e.start[0], off[1] - e.start[1]))
+            end = np.array((off[0] + e.end[0], off[1] - e.end[1]))
             self.draw_line(start, end, layer)
 
     def draw_line(self, start, end, layer):
@@ -105,14 +107,13 @@ class GeneratePcb(pcbnew.ActionPlugin):
         seg = pcbnew.PCB_SHAPE(self.board)
         seg.SetLayer(layer)
         seg.SetShape(pcbnew.S_SEGMENT)
-        seg.SetStart(pcbnew.wxPoint(*map(pcbnew.FromMM, start)))
-        seg.SetEnd(pcbnew.wxPoint(*map(pcbnew.FromMM, end)))
+        seg.SetStart(self.to_point(start))
+        seg.SetEnd(self.to_point(end))
         self.board.Add(seg)
 
     def add_key(self, ref, pos, row_net, col_net, rotation=0):
         """Add a single key to a given (x, y) position"""
         key = pcbnew.FootprintLoad(self.footprint_path, self.footprint_name)
-        position = pcbnew.wxPoint(*map(pcbnew.FromMM, pos.tolist()))
         pads = key.Pads()
         for i in range(1, 4):
             pads[i].SetNet(row_net)
@@ -120,7 +121,7 @@ class GeneratePcb(pcbnew.ActionPlugin):
             pads[i].SetNet(col_net)
         key.SetReference(ref)
         key.Rotate(pcbnew.wxPoint(0, 0), rotation * 10)
-        key.SetPosition(position)
+        key.SetPosition(self.to_point(pos))
         self.board.Add(key)
         self.add_key_tracks(pos, pcbnew.F_Cu, rotation)
 
@@ -150,13 +151,11 @@ class GeneratePcb(pcbnew.ActionPlugin):
             (-0.635, 3.175),
             *self.angled_track_path(
                 np.array((-0.635, 5.08)),
-                np.array((-0.635 + diff[0] / 2, 6.995)),
-                layer,
+                np.array((-0.635 + diff[0] / 2, 6.985)),
             ),
             *self.angled_track_path(
-                np.array((-0.635 + diff[0] / 2, -6.995 + diff[1])),
+                np.array((-0.635 + diff[0] / 2, -6.985 + diff[1])),
                 np.array((-2.54, -5.08)) + diff,
-                layer,
             ),
         ]
         self.add_track_path(path, pos, layer)
@@ -176,8 +175,32 @@ class GeneratePcb(pcbnew.ActionPlugin):
         ]
         self.add_track_path(path, offset, layer, rotation)
 
-    def angled_track_path(self, start, end, layer):
-        """Calculate the points for a angled track path between start and end"""
+    def add_col_connector_tracks(self, col_connector_vals, row_count):
+        """Add tracks going through the column connectors"""
+        off = self.origin_offset
+        layer = pcbnew.B_Cu
+        ql_arc = self.arc_track_path(90, 90)
+        qr_arc = self.arc_track_path(90, 180)
+        h_arc = self.arc_track_path(180, -90)
+        for c_type, pos1, pos2 in col_connector_vals:
+            if c_type == -2:
+                for d in np.linspace(-1, 1, row_count):
+                    self.add_track(pos1 + off + (0, d), pos2 + off + (0, d), layer)
+            else:
+                r = (pos2[0] - pos1[0]) / 3
+                a_r = abs(r)
+                ln = abs(pos1[1] - pos2[1]) - a_r
+                q_arc = qr_arc if c_type == 0 else ql_arc
+                for d in np.linspace(r - 1, r + 1, row_count):
+                    a_d = abs(d)
+                    self.add_track_path(q_arc * a_d, pos1 + off + (0, a_r), layer)
+                    self.add_track(
+                        pos1 + off + (d, a_r), pos1 + off + (d, a_r + ln), layer
+                    )
+                    self.add_track_path(h_arc * a_d, pos2 + off - (r, 0), layer)
+
+    def angled_track_path(self, start, end):
+        """Calculate the points for an angled track path between start and end"""
         diff = end - start
         if abs(diff[0]) > abs(diff[1]):
             d = diff[1] / 2
@@ -189,6 +212,14 @@ class GeneratePcb(pcbnew.ActionPlugin):
             dy = d if np.sign(d) == np.sign(diff[1]) else -d
         return start, start + (dx, dy), end - (dx, dy), end
 
+    def arc_track_path(self, arc_angle, rotation=0):
+        """Calulate the points for path describing an arc with radius 1"""
+        a = np.linspace(
+            rotation, arc_angle + rotation, self.arc_segments * arc_angle // 360
+        )
+        a_rad = np.radians(a)
+        return np.array(list(zip(np.sin(a_rad), np.cos(a_rad))))
+
     def add_track_path(self, path, offset, layer, rotation=0):
         """Add a sequence of tracks defining a path"""
         if rotation != 0:
@@ -199,13 +230,13 @@ class GeneratePcb(pcbnew.ActionPlugin):
         for start, end in zip(path, path[1:]):
             start_pos = r.dot(start) + offset
             end_pos = r.dot(end) + offset
-            self.add_track(start_pos.tolist(), end_pos.tolist(), layer)
+            self.add_track(start_pos, end_pos, layer)
 
     def add_track(self, start, end, layer):
         """Add a single pcb track"""
         track = pcbnew.PCB_TRACK(self.board)
-        track.SetStart(pcbnew.wxPoint(*map(pcbnew.FromMM, start)))
-        track.SetEnd(pcbnew.wxPoint(*map(pcbnew.FromMM, end)))
+        track.SetStart(self.to_point(start))
+        track.SetEnd(self.to_point(end))
         track.SetLayer(layer)
         track.SetWidth(self.track_width)
         self.board.Add(track)
@@ -213,6 +244,10 @@ class GeneratePcb(pcbnew.ActionPlugin):
     def save_board(self, board_file):
         """Save the board to the given output file"""
         pcbnew.SaveBoard(board_file, self.board)
+
+    def to_point(self, pos):
+        """Return a wxPoint with the given position"""
+        return pcbnew.wxPointMM(*pos.tolist())
 
 
 if __name__ == "__main__":
