ad1180a68288c251eca536a79afe94db543a93c4
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-02-18 14:49:09 +0100

    Disable now unused unicode input

--- concavum/qmk/keymaps/default/config.h
+++ concavum/qmk/keymaps/default/config.h
@@ -6,7 +6,4 @@
 #define MATRIX_COLS 12      // column count of both halves combined
 #define THUMB_KEYS 6        // amount of thumb keys of both halves combined
 
-// unicode input mode
-#define UNICODE_SELECTED_MODES UC_LNX
-
 #endif
