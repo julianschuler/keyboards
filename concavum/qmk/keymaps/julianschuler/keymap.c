fbedb18ffda0b965c5baa58c97947f018756efdb
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-02-18 14:46:31 +0100

    Adjust upper layers to other keyboards

--- concavum/qmk/keymaps/julianschuler/keymap.c
+++ concavum/qmk/keymaps/julianschuler/keymap.c
@@ -47,10 +47,6 @@
 #define SS_ALT      LALT_T(DE_SS)
 
 
-// special keys
-#define OMEGA       UC(0x03A9)
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MAX_MATRIX_COLS] = {
 /* layer 0: vou
  * ┌───┬───┬───┬───┬───┬───┐               ┌───┬───┬───┬───┬───┬───┐
@@ -114,40 +110,40 @@ T_ROW(                                          DE_1,       KC_SPC,     _______,
 
 /* layer 3: symbol layer (press symbol layer key to access)
  * ┌───┬───┬───┬───┬───┬───┐               ┌───┬───┬───┬───┬───┬───┐
- * │ ° │ * │ + │ { │ } │ ^ │               │ \ │ < │ > │ = │ - │ § │
+ * │ ° │ + │ < │ ' │ { │ ^ │               │ \ │ } │ & │ > │ - │ § │
  * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
- * │   │ . │ / │ [ │ ] │ ! │               │ ~ │ ( │ ) │ ; │ : │ % │
+ * │ . │ : │ ; │ " │ ( │ ! │               │ ~ │ ) │ = │ / │ * │ ` │
  * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
- * │ € │ µ │ Ω │ @ │ & │ ? │               │ | │ # │ $ │ " │ ' │ ` │
+ * │ µ │ 0 │ @ │ $ │ [ │ ? │               │ | │ ] │ # │ % │ 1 │ € │
  * └───┴───┴───┴─┬─┴─┬─┴─┬─┴─┐           ┌─┴─┬─┴─┬─┴─┬─┴───┴───┴───┘
  *               │   │   │   │           │   │   │   │
  *               │STB│SPC│FN │           │SYM│   │   │
  *               └───┴───┴───┘           └───┴───┴───┘
  */
-[_SL] = LAYER(
-ROW(    DE_DEG,     DE_ASTR,    DE_PLUS,    DE_LCBR,    DE_RCBR,    DE_CIRC,                        DE_BSLS,    DE_LABK,    DE_RABK,    DE_EQL,     DE_MINS,    DE_SECT ),
-ROW(    _______,    DE_DOT,     DE_SLSH,    DE_LBRC,    DE_RBRC,    DE_EXLM,                        DE_TILD,    DE_LPRN,    DE_RPRN,    DE_SCLN,    DE_COLN,    DE_PERC ),
-ROW(    DE_EURO,    DE_MICR,    OMEGA,      DE_AT,      DE_AMPR,    DE_QUES,                        DE_PIPE,    DE_HASH,    DE_DLR,     DE_DQUO,    DE_QUOT,    DE_GRV  ),
+[_SL] = LAYOUT(
+ROW(    DE_DEG,     DE_PLUS,    DE_LABK,    DE_QUOT,    DE_LCBR,    DE_CIRC,                        DE_BSLS,    DE_RCBR,    DE_AMPR,    DE_RABK,    DE_MINS,    DE_SECT ),
+ROW(    DE_DOT,     DE_COLN,    DE_SCLN,    DE_DQUO,    DE_LPRN,    DE_EXLM,                        DE_TILD,    DE_RPRN,    DE_EQL,     DE_SLSH,    DE_ASTR,    DE_GRV  ),
+ROW(    DE_MICR,    DE_0,       DE_AT,      DE_DLR,     DE_LBRC,    DE_QUES,                        DE_PIPE,    DE_RBRC,    DE_HASH,    DE_PERC,    DE_1,       DE_EURO ),
 T_ROW(                                          SFT_TAB,    KC_SPC,     FN_L,                   SYM_L,      _______,    _______                                         )
 ),
 
 
 /* layer 4: number and movement layer (press number layer key to access)
  * ┌───┬───┬───┬───┬───┬───┐               ┌───┬───┬───┬───┬───┬───┐
- * │C_A│C_S│BSP│ ᐃ │DEL│AF4│               │ * │ 7 │ 8 │ 9 │ : │ _ │
+ * │C_G│C_A│BSP│ ᐃ │DEL│AF4│               │ * │ 7 │ 8 │ 9 │ : │ _ │
  * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
- * │   │SFT│ ᐊ │ ᐁ │ ᐅ │PUP│               │ + │ 4 │ 5 │ 6 │ . │BSP│
+ * │C_S│SFT│ ᐊ │ ᐁ │ ᐅ │PUP│               │ + │ 4 │ 5 │ 6 │ . │BSP│
  * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
- * │GUI│CTL│C_G│HOM│END│PDN│               │ / │ 1 │ 2 │ 3 │ , │ - │
+ * │GUI│CTL│INS│HOM│END│PDN│               │ / │ 1 │ 2 │ 3 │ , │ - │
  * └───┴───┴───┴─┬─┴─┬─┴─┬─┴─┐           ┌─┴─┬─┴─┬─┴─┬─┴───┴───┴───┘
  *               │   │   │   │           │   │   │   │
  *               │   │   │NUM│           │FN │SPC│ 0 │
  *               └───┴───┴───┘           └───┴───┴───┘
  */
 [_NL] = LAYER(
-ROW(    CTL_ALT,    CTL_SFT,    KC_BSPC,    KC_UP,      KC_DEL,     ALT_F4,                         DE_ASTR,    DE_7,       DE_8,       DE_9,       DE_COLN,    DE_UNDS ),
-ROW(    _______,    KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGUP,                        DE_PLUS,    DE_4,       DE_5,       DE_6,       DE_DOT,     KC_BSPC ),
-ROW(    KC_LGUI,    KC_LCTL,    CTL_GUI,    KC_HOME,    KC_END,     KC_PGDN,                        DE_SLSH,    DE_1,       DE_2,       DE_3,       DE_COMM,    DE_MINS ),
+ROW(    CTL_GUI,    CTL_ALT,    KC_BSPC,    KC_UP,      KC_DEL,     ALT_F4,                         DE_ASTR,    DE_7,       DE_8,       DE_9,       DE_COLN,    DE_UNDS ),
+ROW(    CTL_SFT,    KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGUP,                        DE_PLUS,    DE_4,       DE_5,       DE_6,       DE_DOT,     KC_BSPC ),
+ROW(    KC_LGUI,    KC_LCTL,    KC_INS,     KC_HOME,    KC_END,     KC_PGDN,                        DE_SLSH,    DE_1,       DE_2,       DE_3,       DE_COMM,    DE_MINS ),
 T_ROW(                                          _______,    _______,    NUM_L,                  FN_L,      KC_SPC,     DE_0                                             )
 ),
 
@@ -167,7 +163,7 @@ T_ROW(                                          _______,    _______,    NUM_L,  
 [_FL] = LAYER(
 ROW(    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                          KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12  ),
 ROW(    WASD,       KC_PAUS,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_BRIU,                        GUI_F1,     GUI_F2,     GUI_F3,     GUI_F4,     GUI_F5,     GUI_F6  ),
-ROW(    RESET,      KC_PAUS,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_BRID,                        GUI_F7,     GUI_F8,     GUI_F9,     GUI_F10,    GUI_F11,    GUI_F12 ),
+ROW(    RESET,      KC_PSCR,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_BRID,                        GUI_F7,     GUI_F8,     GUI_F9,     GUI_F10,    GUI_F11,    GUI_F12 ),
 T_ROW(                                          VOU,        XXXXXXX,    FN_L,                   FN_L,       XXXXXXX,    QWERTZ                                          )
 ),
 };
