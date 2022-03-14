8568267d2e8fb7dc33bd982d31e73ef1622a539e
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-03-14 22:19:32 +0100

    Add files describing MCU and HAL config changes

---
+++ concavum/qmk/halconf.h
@@ -0,0 +1,8 @@
+#ifndef HALCONF_H_CUSTOM
+#define HALCONF_H_CUSTOM
+
+#define HAL_USE_I2C     TRUE
+
+#include_next <halconf.h>
+
+#endif /* HALCONF_H_CUSTOM */
---
+++ concavum/qmk/mcuconf.h
@@ -0,0 +1,9 @@
+#ifndef MCUCONF_H_CUSTOM
+#define MCUCONF_H_CUSTOM
+
+#include_next <mcuconf.h>
+
+#undef RP_I2C_USE_I2C1
+#define RP_I2C_USE_I2C1     TRUE
+
+#endif /* MCUCONF_H_CUSTOM */
