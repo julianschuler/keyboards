dc53a9c14cc22a1fa2041b8d4103671229d01555
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-04-21 21:44:00 +0200

    Change deselecting row behaviour for port expander
    Deselected rows are now set from output to input instead of from low to high.

--- concavum/qmk/mcp2301x.c
+++ concavum/qmk/mcp2301x.c
@@ -20,32 +20,33 @@ void mcp2301x_init(void) {
     i2c_init();
 
     // set pin direction
-    const uint8_t pin_dir[] = { IODIRA, 0b00000000, 0b11111111 };
+    const uint8_t pin_dir[] = { IODIRA, 0b11111111, 0b11111111 };
     mcp2301x_status = i2c_transmit(MCP2301X_ADDR, pin_dir, 3, MCP2301X_TIMEOUT);
     if (mcp2301x_status) return;
 
-    // set pull-up
+    // set pull-ups
     const uint8_t pullup[] = { GPPUA, 0b00000000, 0b11111111 };
     mcp2301x_status = i2c_transmit(MCP2301X_ADDR, pullup, 3, MCP2301X_TIMEOUT);
     if (mcp2301x_status) return;
 
-    // set all outputs high
-    const uint8_t output[] = { GPIOA, 0b11111111 };
+    // set all outputs low
+    const uint8_t output[] = { GPIOA, 0b00000000 };
     mcp2301x_status = i2c_transmit(MCP2301X_ADDR, output, 2, MCP2301X_TIMEOUT);
 }
 
 
-
 uint8_t mcp2301x_read_port(void) {
     uint8_t data = 0xFF;
-    // reading GPIOB (column port) since in mcp23017's/mcp23018's sequential
-    // mode it is addressed directly after writing to GPIOA in select_row()
+    uint8_t gpiob = GPIOB;
+    // first, set address pointer to GPIOB
+    mcp2301x_status = i2c_transmit(MCP2301X_ADDR, &gpiob, 1, MCP2301X_TIMEOUT);
+    // read GPIOB (column port)
     mcp2301x_status = i2c_receive(MCP2301X_ADDR, &data, 1, MCP2301X_TIMEOUT);
     return data;
 }
 
 
 void mcp2301x_write_port(uint8_t data) {
-    const uint8_t port[] = { GPIOA, data };
+    const uint8_t port[] = { IODIRA, data };
     mcp2301x_status = i2c_transmit(MCP2301X_ADDR, port, 2, MCP2301X_TIMEOUT);
 }
