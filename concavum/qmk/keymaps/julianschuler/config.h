9e4e402d21497a49d381e11ace65bfe78d9cbdb6
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-03-27 00:06:48 +0200

    Replace keymap-level info.json files ignored by qmk

--- concavum/qmk/keymaps/julianschuler/config.h
+++ concavum/qmk/keymaps/julianschuler/config.h
@@ -6,4 +6,9 @@
 #define COLS 6         // number of columns per half
 #define THUMB_KEYS 3   // number of thumb keys per half
 
+#define COMBO_TERM 25
+
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 200
+
 #endif
---
+++ ergomin/qmk/keymaps/julianschuler/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H_KEYMAP
+#define CONFIG_H_KEYMAP
+
+#define COMBO_TERM 25
+
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 200
+
+#endif
