d66391e73943f2ee96fc0257ca47c61c4799fda3
Julian Schuler <julianschuler@users.noreply.github.com>
2025-08-10 18:42:44 +0200

    Adjust formatting for thumb keys

--- concavum/qmk/keymaps/default/keymap.c
+++ concavum/qmk/keymaps/default/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC,     DE_Q,       DE_W,       DE_E,       DE_R,       DE_T,                           DE_Z,       DE_U,       DE_I,       DE_O,       DE_P,       DE_UDIA,
 KC_BSPC,    DE_A,       DE_S,       DE_D,       DE_F,       DE_G,                           DE_H,       DE_J,       DE_K,       DE_L,       DE_ODIA,    DE_ADIA,
 KC_LALT,    DE_Y,       DE_X,       DE_C,       DE_V,       DE_B,                           DE_N,       DE_M,       DE_COMM,    DE_DOT,     DE_MINS,    DE_SS,
-                                        TAB_CTL,    L_SHIFT,    NUM_L,                  SYM_L,      KC_SPC,     ENT_GUI
+                                          TAB_CTL,    L_SHIFT,    NUM_L,              SYM_L,      KC_SPC,     ENT_GUI
 ),
 
 
@@ -81,7 +81,7 @@ KC_LALT,    DE_Y,       DE_X,       DE_C,       DE_V,       DE_B,               
 KC_ESC,     KC_TAB,     DE_Q,       DE_W,       DE_E,       DE_R,                           DE_T,       DE_Z,       DE_U,       DE_I,       DE_O,       DE_P,
 KC_LCTL,    KC_LSFT,    DE_A,       DE_S,       DE_D,       DE_F,                           DE_G,       DE_H,       DE_J,       DE_K,       DE_L,       DE_ODIA,
 KC_LALT,    KC_LCTL,    DE_Y,       DE_X,       DE_C,       DE_V,                           DE_B,       DE_N,       DE_M,       DE_COMM,    DE_DOT,     DE_MINS,
-                                        DE_1,       KC_SPC,     LNUM_L,                 _______,    XXXXXXX,    KC_ENT
+                                          DE_1,       KC_SPC,     LNUM_L,             _______,    XXXXXXX,    KC_ENT
 ),
 
 
@@ -101,7 +101,7 @@ KC_LALT,    KC_LCTL,    DE_Y,       DE_X,       DE_C,       DE_V,               
 _______,    _______,    DE_4,       DE_5,       DE_6,       _______,                        _______,    _______,    _______,    _______,    _______,    _______,
 _______,    DE_0,       DE_1,       DE_2,       DE_3,       _______,                        _______,    _______,    _______,    _______,    _______,    _______,
 _______,    _______,    DE_7,       DE_8,       DE_9,       _______,                        _______,    _______,    _______,    _______,    _______,    _______,
-                                        _______,    _______,    LNUM_L,                 FN_L,        _______,    _______
+                                          _______,    _______,    LNUM_L,             FN_L,        _______,    _______
 ),
 
 
@@ -121,7 +121,7 @@ _______,    _______,    DE_7,       DE_8,       DE_9,       _______,            
 DE_DEG,     DE_PLUS,    DE_LABK,    DE_QUOT,    DE_LCBR,    DE_CIRC,                        DE_BSLS,    DE_RCBR,    DE_AMPR,    DE_RABK,    DE_MINS,    DE_SECT,
 DE_DOT,     DE_COLN,    DE_SCLN,    DE_DQUO,    DE_LPRN,    DE_EXLM,                        DE_TILD,    DE_RPRN,    DE_EQL,     DE_SLSH,    DE_ASTR,    DE_GRV,
 DE_MICR,    DE_0,       DE_AT,      DE_DLR,     DE_LBRC,    DE_QUES,                        DE_PIPE,    DE_RBRC,    DE_HASH,    DE_PERC,    DE_1,       DE_EURO,
-                                        SFT_TAB,    KC_SPC,     FN_L,                   SYM_L,      _______,    _______
+                                          SFT_TAB,    KC_SPC,     FN_L,               SYM_L,      _______,    _______
 ),
 
 
@@ -141,7 +141,7 @@ DE_MICR,    DE_0,       DE_AT,      DE_DLR,     DE_LBRC,    DE_QUES,            
 CTL_GUI,    CTL_ALT,    KC_BSPC,    KC_UP,      KC_DEL,     ALT_F4,                         DE_ASTR,    DE_7,       DE_8,       DE_9,       DE_COLN,    DE_UNDS,
 CTL_SFT,    KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGUP,                        DE_PLUS,    DE_4,       DE_5,       DE_6,       DE_DOT,     KC_BSPC,
 KC_LGUI,    KC_LCTL,    KC_INS,     KC_HOME,    KC_END,     KC_PGDN,                        DE_SLSH,    DE_1,       DE_2,       DE_3,       DE_COMM,    DE_MINS,
-                                        _______,    _______,    NUM_L,                  FN_L,      KC_SPC,     DE_0
+                                          _______,    _______,    NUM_L,              FN_L,      KC_SPC,     DE_0
 ),
 
 
@@ -161,6 +161,6 @@ KC_LGUI,    KC_LCTL,    KC_INS,     KC_HOME,    KC_END,     KC_PGDN,            
 KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                          KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
 GAME_L,     KC_PAUS,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_BRIU,                        GUI_F1,     GUI_F2,     GUI_F3,     GUI_F4,     GUI_F5,     GUI_F6,
 QK_BOOT,    KC_PSCR,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_BRID,                        GUI_F7,     GUI_F8,     GUI_F9,     GUI_F10,    GUI_F11,    GUI_F12,
-                                        XXXXXXX,    XXXXXXX,    FN_L,                   FN_L,       XXXXXXX,    XXXXXXX
+                                          XXXXXXX,    XXXXXXX,    FN_L,               FN_L,       XXXXXXX,    XXXXXXX
 ),
 };
--- concavum/qmk/keymaps/julianschuler/keymap.c
+++ concavum/qmk/keymaps/julianschuler/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC,     DE_V,       DE_DOT,     DE_O,       DE_U,       DE_ADIA,                        DE_Q,       DE_G,       DE_L,       DE_H,       DE_F,       DE_UNDS,
 KC_BSPC,    DE_C,       DE_A,       DE_E,       DE_I,       DE_UDIA,                        DE_B,       DE_T,       DE_R,       DE_N,       DE_S,       DE_J,
 SS_ALT,     DE_Z,       DE_X,       DE_COMM,    DE_Y,       DE_ODIA,                        DE_P,       DE_D,       DE_W,       DE_M,       DE_K,       DE_MINS,
-                                        TAB_CTL,    L_SHIFT,    NUM_L,                  SYM_L,      KC_SPC,     ENT_GUI
+                                          TAB_CTL,    L_SHIFT,    NUM_L,              SYM_L,      KC_SPC,     ENT_GUI
 ),
 
 
@@ -86,7 +86,7 @@ SS_ALT,     DE_Z,       DE_X,       DE_COMM,    DE_Y,       DE_ODIA,            
 KC_ESC,     DE_Q,       DE_W,       DE_E,       DE_R,       DE_T,                           DE_Z,       DE_U,       DE_I,       DE_O,       DE_P,       DE_UDIA,
 KC_BSPC,    DE_A,       DE_S,       DE_D,       DE_F,       DE_G,                           DE_H,       DE_J,       DE_K,       DE_L,       DE_ODIA,    DE_ADIA,
 KC_LALT,    DE_Y,       DE_X,       DE_C,       DE_V,       DE_B,                           DE_N,       DE_M,       DE_COMM,    DE_DOT,     DE_MINS,    DE_SS,
-                                        TAB_CTL,    L_SHIFT,    NUM_L,                  SYM_L,      KC_SPC,     ENT_GUI
+                                          TAB_CTL,    L_SHIFT,    NUM_L,              SYM_L,      KC_SPC,     ENT_GUI
 ),
 
 
@@ -106,7 +106,7 @@ KC_LALT,    DE_Y,       DE_X,       DE_C,       DE_V,       DE_B,               
 KC_ESC,     KC_TAB,     DE_Q,       DE_W,       DE_E,       DE_R,                           DE_T,       DE_Z,       DE_U,       DE_I,       DE_O,       DE_P,
 KC_LCTL,    KC_LSFT,    DE_A,       DE_S,       DE_D,       DE_F,                           DE_G,       DE_H,       DE_J,       DE_K,       DE_L,       DE_ODIA,
 KC_LALT,    KC_LCTL,    DE_Y,       DE_X,       DE_C,       DE_V,                           DE_B,       DE_N,       DE_M,       DE_COMM,    DE_DOT,     DE_MINS,
-                                        DE_1,       KC_SPC,     LNUM_L,                 _______,    XXXXXXX,    KC_ENT
+                                          DE_1,       KC_SPC,     LNUM_L,             _______,    XXXXXXX,    KC_ENT
 ),
 
 
@@ -126,7 +126,7 @@ KC_LALT,    KC_LCTL,    DE_Y,       DE_X,       DE_C,       DE_V,               
 _______,    _______,    DE_4,       DE_5,       DE_6,       _______,                        _______,    _______,    _______,    _______,    _______,    _______,
 _______,    DE_0,       DE_1,       DE_2,       DE_3,       _______,                        _______,    _______,    _______,    _______,    _______,    _______,
 _______,    _______,    DE_7,       DE_8,       DE_9,       _______,                        _______,    _______,    _______,    _______,    _______,    _______,
-                                        _______,    _______,    LNUM_L,                 FN_L,        _______,    _______
+                                          _______,    _______,    LNUM_L,             FN_L,        _______,    _______
 ),
 
 
@@ -146,7 +146,7 @@ _______,    _______,    DE_7,       DE_8,       DE_9,       _______,            
 DE_DEG,     DE_PLUS,    DE_LABK,    DE_QUOT,    DE_LCBR,    DE_CIRC,                        DE_BSLS,    DE_RCBR,    DE_AMPR,    DE_RABK,    DE_MINS,    DE_SECT,
 DE_DOT,     DE_COLN,    DE_SCLN,    DE_DQUO,    DE_LPRN,    DE_EXLM,                        DE_TILD,    DE_RPRN,    DE_EQL,     DE_SLSH,    DE_ASTR,    DE_GRV,
 DE_MICR,    DE_0,       DE_AT,      DE_DLR,     DE_LBRC,    DE_QUES,                        DE_PIPE,    DE_RBRC,    DE_HASH,    DE_PERC,    DE_1,       DE_EURO,
-                                        SFT_TAB,    KC_SPC,     FN_L,                   SYM_L,      _______,    _______
+                                          SFT_TAB,    KC_SPC,     FN_L,               SYM_L,      _______,    _______
 ),
 
 
@@ -166,7 +166,7 @@ DE_MICR,    DE_0,       DE_AT,      DE_DLR,     DE_LBRC,    DE_QUES,            
 CTL_GUI,    CTL_ALT,    KC_BSPC,    KC_UP,      KC_DEL,     ALT_F4,                         DE_ASTR,    DE_7,       DE_8,       DE_9,       DE_COLN,    DE_UNDS,
 CTL_SFT,    KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGUP,                        DE_PLUS,    DE_4,       DE_5,       DE_6,       DE_DOT,     KC_BSPC,
 KC_LGUI,    KC_LCTL,    KC_INS,     KC_HOME,    KC_END,     KC_PGDN,                        DE_SLSH,    DE_1,       DE_2,       DE_3,       DE_COMM,    DE_MINS,
-                                        _______,    _______,    NUM_L,                  FN_L,      KC_SPC,     DE_0
+                                          _______,    _______,    NUM_L,              FN_L,       KC_SPC,     DE_0
 ),
 
 
@@ -186,7 +186,7 @@ KC_LGUI,    KC_LCTL,    KC_INS,     KC_HOME,    KC_END,     KC_PGDN,            
 KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                          KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
 GAME_L,     KC_PAUS,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_BRIU,                        GUI_F1,     GUI_F2,     GUI_F3,     GUI_F4,     GUI_F5,     GUI_F6,
 QK_BOOT,    KC_PSCR,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_BRID,                        GUI_F7,     GUI_F8,     GUI_F9,     GUI_F10,    GUI_F11,    GUI_F12,
-                                        VOU,        XXXXXXX,    FN_L,                   FN_L,       XXXXXXX,    QWERTZ
+                                          VOU,        XXXXXXX,    FN_L,               FN_L,       XXXXXXX,    QWERTZ
 ),
 };
 
