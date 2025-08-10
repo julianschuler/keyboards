8f99fa83ffb8fb65bc571481417fc9ed463bbc08
Julian Schuler <julianschuler@users.noreply.github.com>
2025-08-10 21:25:17 +0200

    Add QMK config files generated by the customizer

---
+++ concavum-v2/qmk/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// activate entering the boot loader by double tapping the reset button
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+// split keyboard settings
+#define USB_VBUS_PIN GP29
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP2
+#define SERIAL_USART_RX_PIN GP3
+#define SERIAL_USART_PIN_SWAP
+
+#endif
